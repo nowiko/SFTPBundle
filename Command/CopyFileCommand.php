332dc801a28454e035b247c3b46c4a8e25fcb645
Novikov Viktor <viktor.novikov95@gmail.com>
2017-10-03 21:09:04 +0300

    Finished send/copy commands

--- Command/CopyFileCommand.php
+++ Command/CopyFileCommand.php
@@ -2,6 +2,7 @@
 
 namespace SF2Helpers\SFTPBundle\Command;
 
+use SF2Helpers\SFTPBundle\SFTP\SFTP;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,9 +20,9 @@ class CopyFileCommand extends ContainerAwareCommand
             ))
             ->setDescription('Copy file from remote SFTP server to local machine')
             ->setHelp("
-The <info>./app/console sftp:copy</info> command copies file from remote SFTP server to your local machine by specified path:
-Command example:
-  <info>./app/console sftp:copy /path/to/remoteFile.txt /path/to/localFile.txt</info>
+                The <info>./app/console sftp:copy</info> command copies file from remote SFTP server to your local machine by specified path:
+                Command example:
+                  <info>./app/console sftp:copy /path/to/remoteFile.txt /path/to/localFile.txt</info>
             ");
     }
 
@@ -30,6 +31,14 @@ Command example:
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var SFTP $sftp */
         $sftp = $this->get('sftp');
+
+        try {
+            $sftp->copy($input->getArgument('remoteFile'), $input->getArgument('localFile'));
+            $output->writeln('File transfer handled.');
+        } catch (\Exception $e) {
+            $output->writeln('File transfer failed.');
+        }
     }
 }
--- Command/SendFileCommand.php
+++ Command/SendFileCommand.php
@@ -2,6 +2,7 @@
 
 namespace SF2Helpers\SFTPBundle\Command;
 
+use SF2Helpers\SFTPBundle\SFTP\SFTP;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,9 +20,9 @@ class SendFileCommand extends ContainerAwareCommand
             ))
             ->setDescription('Send file to remote SFTP server from local machine')
             ->setHelp("
-The <info>./app/console sftp:send</info> command copies file to remote SFTP server from your local machine by specified path:
-Command example:
-  <info>./app/console sftp:send /path/to/localFile.txt /path/to/remoteFile.txt</info>
+                The <info>./app/console sftp:send</info> command copies file to remote SFTP server from your local machine by specified path:
+                Command example:
+                  <info>./app/console sftp:send /path/to/localFile.txt /path/to/remoteFile.txt</info>
             ");
     }
 
@@ -30,6 +31,14 @@ Command example:
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var SFTP $sftp */
         $sftp = $this->get('sftp');
+
+        try {
+            $sftp->send($input->getArgument('localFile'), $input->getArgument('remoteFile'));
+            $output->writeln('File transfer handled.');
+        } catch (\Exception $e) {
+            $output->writeln('File transfer failed.');
+        }
     }
 }
